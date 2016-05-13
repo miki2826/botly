db4c846779317df43d50f3e950eadec31b4948d9
miki2826 <miki2826@gmail.com>
2016-05-13 15:11:28 +0300

    added test task to grunt

--- build/aliases.js
+++ build/aliases.js
@@ -3,6 +3,9 @@ module.exports = function (grunt, options) {
     var tasks = ["jshint", "env", "instrument", "mochaTest", "storeCoverage", "makeReport"];
     return {
         "tasks": ["availabletasks"],
-        "default": tasks
+        "default": tasks,
+        "test": [
+            "env", "instrument", "mochaTest", "storeCoverage", "makeReport"
+        ]
     };
 };
